7cfaf0d19230feac63608e7f89c101c5acb4527b
Carlos Jafet Neto <cjafet07@gmail.com>
2020-03-23 16:12:12 -0300

    Add user controller

---
+++ src/main/java/mvcrestdto/demo/controller/UserController.java
@@ -0,0 +1,14 @@
+package mvcrestdto.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/health")
+    String status() {
+        return "Health!";
+    }
+
+}
