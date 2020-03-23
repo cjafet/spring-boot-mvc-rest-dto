e9b983775a12e06e89770cc2f4ceafa9fe1b9f56
Carlos Jafet Neto <cjafet07@gmail.com>
2020-03-23 19:30:17 -0300

    Fix path

--- src/main/java/mvcrestdto/demo/controller/CompanyController.java
+++ src/main/java/mvcrestdto/demo/controller/CompanyController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CompanyController {
-    @RequestMapping("/health")
+    @RequestMapping("/company")
     String companies() {
         return "Company";
     }
